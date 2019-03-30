50247e0ccb7d83d76e9df82a209b6aa205a5882b
liangfengbo <itbo@163.com>
2019-03-30 18:26:50 +0800

    去掉CDN打包

--- web/config/index.js
+++ web/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://images.boblog.com/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
