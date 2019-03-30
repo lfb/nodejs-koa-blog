6c05005eb6adc24a043f15347bc1eb16dcffe99e
liangfengbo <itbo@163.com>
2019-03-30 21:45:30 +0800

    Test.1.0.2

--- admin/config/index.js
+++ admin/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
