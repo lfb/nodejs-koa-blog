16a8ebec16e43bceae26d413dcc2426f41704de4
liangfengbo <itbo@163.com>
2019-04-05 21:41:40 +0800

    修复JWT验证 && 修复跨域问题

---
+++ middleware/JWTPath.js
@@ -0,0 +1,12 @@
+module.exports = [
+    // 文章列表
+    "/api/v1/article/list",
+    "/api/v1/article/detail",
+    "/api/v1/article/search",
+    "/api/v1/upload/token",
+    "/api/v1/user/register",
+    "/api/v1/user/login",
+    "/api/v1/category/list",
+    "/api/v1/category/article/list",
+    "/api/v1/category/article"
+]
--- middleware/JWTToken.js
+++ middleware/JWTToken.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 const secret = require('../config/secret')
 const util = require('util')
 const verify = util.promisify(jwt.verify)
+const JWTPath = require('./JWTPath')
+
 
 /**
  * 判断token是否可用
@@ -9,15 +11,21 @@ const verify = util.promisify(jwt.verify)
 module.exports = function () {
     return async function (ctx, next) {
 
-        // 检查此次请求中是否带有 token，如果没有则抛出异常。
-        const token = ctx.header.authorization;
+        // 检测过滤的路由就不做解析JWT了
+        if (JWTPath.find(item => item === ctx.request.url)) {
+            await next()
+
+            return false;
+        }
 
         try {
-
+            // 获取jwt
+            const token = ctx.header.authorization
             if (token) {
+                let payload
                 try {
-                    // 解密payload，获取用户名，邮箱和ID
-                    let payload = await verify(token.split(' ')[1], secret.sign)
+                    // 解密payload，获取用户名和ID
+                    payload = await verify(token.split(' ')[1], secret.sign)
                     ctx.user = {
                         name: payload.name,
                         email: payload.email,
@@ -28,32 +36,28 @@ module.exports = function () {
                     ctx.status = 401;
                     ctx.body = {
                         code: 401,
-                        message: 'Token is Expired  Token已过期，需要重新登录！',
-                        err
+                        message: "Token身份无效!"
                     }
-
                     return false;
                 }
             }
             await next()
 
         } catch (err) {
-
             if (err.status === 401) {
                 ctx.status = 401;
                 ctx.body = {
                     code: 401,
-                    message: 'Authentication Error 需要在request头附带Authorization:Bearer [token]字段',
-                    err
+                    message: "401 Unauthorized，请求要求用户的身份认证!"
                 }
             } else {
                 ctx.status = 500;
                 ctx.body = {
                     code: 500,
-                    message: 'Authentication Error 身份验证错误',
-                    err
+                    message: err
                 }
             }
+
         }
     }
 }
