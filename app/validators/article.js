59c84fded9580a7bf0f9e32a92d49e7d1e17f2b9
liangfengbo <itbo@163.com>
2019-05-13 21:42:47 +0800

    修改项目名称

--- app/validators/article.js
+++ app/validators/article.js
@@ -6,18 +6,22 @@ const {
 class ArticleValidator extends LinValidator {
     constructor() {
         super()
-        this.title = [new Rule("isLength", "文章标题不能为空", {
-            min: 1
-        })]
-        this.author = [new Rule("isLength", "文章作者不能为空", {
-            min: 1
-        })]
-        this.content = [new Rule("isLength", "文章内容不能为空", {
-            min: 1
-        })]
-        this.cover = [new Rule("isLength", "文章封面不能为空", {
-            min: 1
-        })]
+        this.title = [
+            new Rule("isLength", "文章标题不能为空", {
+                min: 1
+            })]
+        this.author = [
+            new Rule("isLength", "文章作者不能为空", {
+                min: 1
+            })]
+        this.content = [
+            new Rule("isLength", "文章内容不能为空", {
+                min: 1
+            })]
+        this.cover = [
+            new Rule("isLength", "文章封面不能为空", {
+                min: 1
+            })]
     }
 }
 
