461c31ec1ff9deedd1255b2442308b1ece91c81a
liangfengbo <itbo@163.com>
2019-06-17 17:17:28 +0800

    更新打包后台文件

--- admin/config/index.js
+++ admin/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://cdn.boblog.com/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
