e715fdea8f3d1aa3e209e9793ee70d93225da67a
liangfengbo <itbo@163.com>
2019-06-17 16:32:31 +0800

    修复CDN

--- admin/config/index.js
+++ admin/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'http://cdn.boblog.com/',
 
     /**
      * Source Maps
--- web/src/libs/util.js
+++ web/src/libs/util.js
@@ -6,7 +6,7 @@ const ajaxUrl = process.env.NODE_ENV === 'development'
   // 测试环境api接口
   ? 'http://localhost:3000/v1'
   // 线上环境api接口
-  : 'http://api.boblog.com/api/v1';
+  : 'http://api.boblog.com/v1';
 
 util.ajax = axios.create({
   baseURL: ajaxUrl,
