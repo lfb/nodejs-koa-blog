fbc5cf013cebfc84975c8fe50c8b022a4f998d7e
Fengbo Liang <itbo@163.com>
2021-07-21 12:50:33 +0800

    fix：新增评论接口

--- frontend-blog/pages/article/detail.vue
+++ frontend-blog/pages/article/detail.vue
@@ -32,6 +32,37 @@
         </div>
       </div>
     </div>
+
+
+    <div class="comment">
+
+      <el-button type="primary" icon="el-icon-view" @click="onPreComment">预览评论</el-button>
+
+      <div>
+        请输入内容，支持Markdown语法
+      </div>
+      <el-input
+        v-model="comment"
+        type="textarea"
+        :rows="2"
+        placeholder="请输入内容">
+      </el-input>
+      <el-button type="primary" @click="submitComment">提交</el-button>
+    </div>
+
+    <div class="comment" v-html="preCommentContent"></div>
+
+
+    <div class="comment-list">
+
+      <ul class="comment-box">
+        <li v-for="item in commentList.data" :key="item.id" class="comment-item">
+          {{item.user_info  || '匿名哥哥'}}：
+         <div v-html="mdRender(item.content)"></div> （{{item.created_at}}）
+        </li>
+      </ul>
+    </div>
+
     <div class="copyright">
       &copy;本文原创发布于波波博客 - <a href="http://www.boblog.com">BoBlog.com</a>，转载请注明出处，谢谢合作！
     </div>
@@ -39,6 +70,25 @@
 </template>
 <script>
 import {getArticleDetail} from '@/request/api/article'
+import {getCommentTarget, createComment} from "@/request/api/comment";
+
+const hljs = require('highlight.js');
+const md = require('markdown-it')({
+  highlight (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+          hljs.highlight(str, {
+            language: lang,
+            ignoreIllegals: true
+          }).value + '</code></pre>';
+      } catch (__) {}
+    }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+});
+
 export default {
   name: "ArticleDetail",
   async asyncData(context) {
@@ -50,13 +100,51 @@ export default {
     const [err, res] = await getArticleDetail(params)
     if(!err) {
       return {
+        id,
+        commentList: [],
         article: res.data.data
       }
     }
   },
   data() {
     return {
+      comment: '',
+      preCommentContent: '',
       article: null,
+    }
+  },
+  mounted() {
+    this.getComment()
+  },
+  methods:{
+    onPreComment() {
+      this.preCommentContent = this.mdRender(this.comment)
+    },
+    mdRender(content) {
+      return md.render(content)
+    },
+    async submitComment() {
+      const [err, data] = await createComment({
+        user_id: 0,
+        article_id: this.id,
+        content: this.comment
+      })
+      if(!err) {
+        console.log(data)
+        this.$message.success('评论成功！')
+        await this.getComment()
+      }
+    },
+    async getComment() {
+      const [err, res] = await getCommentTarget({
+        article_id: this.id,
+        is_replay: 1,
+        is_user: 1
+      })
+      if(!err) {
+        console.log('res.data.data', res.data.data)
+        this.commentList = res.data.data
+      }
     }
   }
 };
---
+++ frontend-blog/request/urls/comment.js
@@ -0,0 +1,5 @@
+
+export default {
+  create: '/comment',
+  target: '/comment/target/list',
+}
